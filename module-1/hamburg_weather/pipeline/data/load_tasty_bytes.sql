ebec066b2e5cee839b8cb528e7341c4ea6f92fe1
Tsetsi Naydenov (SFS IT CRR) <tsvetelin.naydenov@siemens.com>
2025-12-07 22:19:30 +0100

    dfg

--- module-1/hamburg_weather/pipeline/data/load_tasty_bytes.sql
+++ module-1/hamburg_weather/pipeline/data/load_tasty_bytes.sql
@@ -65,7 +65,7 @@ COPY INTO {{env}}_tasty_bytes.raw_pos.country
    iso_country VARCHAR(2),
    city VARCHAR(16777216),
    city_population VARCHAR(16777216),
-   city_id NUMBER(19,0)
+   city_id NUMBER(18,0)
 );
 
 
